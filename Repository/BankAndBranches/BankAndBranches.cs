e0411c931c884aeceef42b58e1979a28e2fd491e
Biswadip <biswadip100@gmail.com>
2026-08-15 20:21:51 +0530

    Added interface BankAndBranches. Created class BankResponse and BranchResponse

---
+++ Repository/BankAndBranches/BankAndBranches.cs
@@ -0,0 +1,15 @@
+﻿using Domain.BankAndBranches;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.BankAndBranches
+{
+    public interface BankAndBranches
+    {
+        Task<List<BankResponse>> GetBanks();
+        Task<List<BranchResponse>> GetBranchesByBankId(int bankId);
+        Task<BankResponse> GetBankByBankId(int bankId);
+        Task<string> Save(BankResponse bank);
+    } // BankAndBranches...
+}
