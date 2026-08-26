3ec2b8a698b5e7c6ab98190ce4edd91172b83a0d
Biswadip <biswadip100@gmail.com>
2026-08-26 13:27:11 +0530

    create SalaryComponentResponse class

---
+++ Domain/SalaryComponent/SalaryComponentResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.SalaryComponent
+{
+    public class SalaryComponentResponse
+    {
+        public int ComponentId {  get; set; }
+        public int CompanyId {  get; set; }
+        public string ComponentName {  get; set; } = string.Empty;
+        public string ComponentType { get; set; } = string.Empty;
+        public string CalculationType {  get; set; } = string.Empty;
+        public string Taxable { get; set; } = string.Empty;
+        public string IsActive { get; set; } = string.Empty;
+    } // SalaryComponentResponse...
+}
